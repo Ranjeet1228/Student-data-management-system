d245d9c38629d37b229e1d33605f03085ab5e9b1
Ranjeet upadhyay <upadhyayr0728@gmail.com>
2025-08-30 22:44:45 +0530

    Update dataManagement.java

--- project student database/dataManagement.java
+++ project student database/dataManagement.java
@@ -8,11 +8,11 @@ public class dataManagement {
     //1
     public void addstudent(Scanner sc){
          
-
-        System.out.println("Enter the no of student:- ");
+        System.out.println("\n Enter the no of student:- ");
         int number = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < number; i++) {
+            System.out.println("Enter the data of "+ (i+1) +" student:- ");
             System.out.print("Enter name:- ");
             String name = sc.nextLine();
             System.out.print("Enter RollNo:- ");
@@ -38,17 +38,22 @@ public class dataManagement {
     public void update(Scanner sc){
 
 
-        System.out.println("Are you want any modication:- ");
+        System.out.println("\n Are you want any modication:- ");
         boolean ch = sc.nextBoolean();
+        sc.nextLine();
+
         while (ch) { 
             System.out.println("Enter roll number:- ");
             long rollNum = sc.nextLong();
+            sc.nextLine();
 
             for(Student s : st){
                  if(s.getRollNo() == rollNum){
 
                      System.out.println("Enter new name:- ");
+                     sc.nextLine();
                      s.setname(sc.nextLine());
+                    //  sc.nextLine();
 
                      System.out.print("Enter new Roll number:- ");
                      s.setRollNo(sc.nextLong());
@@ -60,21 +65,22 @@ public class dataManagement {
 
                      System.out.print("Enter new course datail:- ");
                      s.setcourse(sc.nextLine());
-                     sc.nextLine();
+                     //sc.nextLine();
 
                      System.out.print("Enter new marks of that student:- ");
                      s.setmarks(sc.nextDouble());
                      sc.nextLine();
                  }
+                 System.out.println();
             }
 
-            System.out.println("Any more modifation, Enter Y for yes and N for no:- ");
+            System.out.println("\nAny more modifation, Enter Y for yes and N for no:- ");
             String c;
             do { 
                 c= sc.next();
-                if(c.equals("Y")){
+                if(c.equalsIgnoreCase("Y")){
                     ch = true;
-                } else if(c.equals("n")){
+                } else if(c.equalsIgnoreCase("N")){
                     ch = false;
                 } else {
                     System.out.println("Enter again:- ");
@@ -83,7 +89,6 @@ public class dataManagement {
 
         }
 
-        sc.close();
     }
 
 
@@ -91,18 +96,23 @@ public class dataManagement {
     public void printData(Scanner sc){
 
         if(st.isEmpty()){
-            System.out.println("Error! No data found.");
+            System.out.println("\nError! No data found.");
             return;
         }
         
         System.out.print("Enter roll No:- ");
         long Roll = sc.nextLong();
 
+        System.out.println();
         for(Student student : st){
             if(student.getRollNo()==Roll){
+                System.out.println("............");
                 System.out.println(student);
+                System.out.println("...........");
+                return;
             }
         }
+        System.out.println("...... Error! not found.......");
 
        
     }
@@ -113,7 +123,7 @@ public class dataManagement {
 
 
         if(st.isEmpty()){
-            System.out.println("Error!, No data found.");
+            System.out.println("\nError!, No data found.");
             return;
         }
 
@@ -122,11 +132,13 @@ public class dataManagement {
         
         for(Student s: st){
             if(s.getRollNo() == roll){
-                System.out.println("Found: "+ s.getname());
+                System.out.println("...........");
+                System.out.println("\nFound: "+ s.getname());
+                System.out.println("...........");
                 return;
             }
         }
-        System.out.println("NOT FOUND!");
+        System.out.println("\n NOT FOUND!");
     }
 
 
